7c760b971d0b5551d0e5456ed15c1a859eecfb3a
mhaikal <mhaikallokman@gmail.com>
2022-03-14 17:58:55 +0800

    Distribute Page ui and controller

---
+++ src/main/controller/DistributePageController.java
@@ -0,0 +1,39 @@
+package main.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+public class DistributePageController {
+
+    @FXML
+    private Text tSelectedDonor;
+
+    @FXML
+    private Text tSelectedNGO;
+
+    @FXML
+    private TableView<?> tvMain;
+
+    @FXML
+    void btnMatchAidsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewAidsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewDonorClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewNGOClicked(ActionEvent event) {
+
+    }
+
+}
