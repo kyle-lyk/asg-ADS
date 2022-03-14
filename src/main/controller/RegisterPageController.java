fec62c4c85972950760bcc82b87862ab6257f786
Kyle <kyle.yk@outlook.com>
2022-03-14 14:19:45 +0800

    refactor: Changed in UI and Controller Settings

--- src/main/controller/RegisterPageController.java
+++ src/main/controller/RegisterPageController.java
@@ -1,11 +1,19 @@
 package main.controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.model.Manager;
 
 public class RegisterPageController {
 
@@ -26,5 +34,16 @@ public class RegisterPageController {
 
     @FXML
     private TextField usernameField;
+
+    @FXML
+    void switch_to_LoginPage(ActionEvent event) {
+        try{
+            Stage mainStage = Manager.getInstance().getStage();
+            Parent root = FXMLLoader.load(getClass().getResource("/main/view/LoginPage.fxml"));
+            mainStage.setScene(new Scene(root, 1280, 720));
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
     
 }
