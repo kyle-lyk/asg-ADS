34489560288937702b161c392f74c282300bf3b2
mhaikal <mhaikallokman@gmail.com>
2022-03-28 02:53:59 +0800

    JavaDoc comments, fixed lowercase bug

--- src/main/model/AidList.java
+++ src/main/model/AidList.java
@@ -2,23 +2,19 @@ package main.model;
 
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Table formats for Distribution System.
+ */
 public class AidList {
     private SimpleStringProperty donor, phone, aids, ngo;
     private int quantity, manpower;
-    private String filterItem;
 
     private int rowNum;
 
-
-    public String getFilterItem() {
-        return this.filterItem;
-    }
-
-    public void setFilterItem(String filterItem) {
-        this.filterItem = filterItem;
-    }
-
-    // constructor for Matched Aids table content
+    /**
+     * Constructs the content for
+     * the rows of MatchedAidHistory table.
+     */
     public AidList(String donor, String phone, String aids, Integer quantity, String ngo, Integer manpower) {
         this.donor = new SimpleStringProperty(donor);
         this.phone = new SimpleStringProperty(phone);
@@ -28,7 +24,10 @@ public class AidList {
         this.manpower = manpower;
     }
 
-    // constructor for NGO table content
+    /**
+     * Constructs the content for
+     * the rows of NGO table.
+     */
     public AidList(Integer rowNum, String ngo, Integer manpower, String aids, Integer quantity) {
         this.rowNum = rowNum;
         this.aids = new SimpleStringProperty(aids);
@@ -37,7 +36,10 @@ public class AidList {
         this.manpower = manpower;
     }
 
-    // constructor for Donor table content
+    /**
+     * Constructs the content for
+     * the rows of Donor table.
+     */
     public AidList(Integer rowNum, String donor, String phone, String aids, Integer quantity) {
         this.rowNum = rowNum;
         this.donor = new SimpleStringProperty(donor);
@@ -46,61 +48,116 @@ public class AidList {
         this.quantity = quantity;
     }
 
+    /**
+     * Gets the donor name.
+     * @return donor's name
+     */
     public String getDonor() {
         return this.donor.get();
     }
 
+    /**
+     * Sets the name of the donor.
+     * @param donor donor's name
+     */
     public void setDonor(SimpleStringProperty donor) {
         this.donor = donor;
     }
 
+    /**
+     * Gets the current table row number.
+     * @return table row number
+     */
     public int getRowNum() {
         return this.rowNum;
     }
 
+    /**
+     * Sets the table row number.
+     * @param rowNum row number
+     */
     public void setRowNum(Integer rowNum) {
         this.rowNum = rowNum;
     }
 
+    /**
+     * Gets the phone number of donor.
+     * @return phone number
+     */
     public String getPhone() {
         return this.phone.get();
     }
 
+    /**
+     * Sets the phone number of donor.
+     * @param phone phone number
+     */
     public void setPhone(SimpleStringProperty phone) {
         this.phone = phone;
     }
 
+    /**
+     * Gets the name of aid.
+     * @return aid name
+     */
     public String getAids() {
         return this.aids.get();
     }
 
+    /**
+     * Sets the name of aid.
+     * @param aids aid name
+     */
     public void setAids(SimpleStringProperty aids) {
         this.aids = aids;
     }
 
+    /**
+     * Gets the NGO's name.
+     * @return NGO name
+     */
     public String getNgo() {
         return this.ngo.get();
     }
 
+    /**
+     * Sets the NGO's name.
+     * @param ngo NGO's name
+     */
     public void setNgo(SimpleStringProperty ngo) {
         this.ngo = ngo;
     }
 
+    /**
+     * Gets the quantity of aid.
+     * @return aid quantity
+     */
     public int getQuantity() {
         return this.quantity;
     }
 
+    /**
+     * Sets the quatity of aid.
+     * @param quantity aid quantity
+     */
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Gets the NGO's manpower amount.
+     * @return NGO's manpower amount
+     */
     public int getManpower() {
         return this.manpower;
     }
 
+    /**
+     * Sets the NGO's manpower amount.
+     * @param manpower NGO's manpower amount
+     */
     public void setManpower(Integer manpower) {
         this.manpower = manpower;
     }
-
 
 }
