630acbeec8a258376da60ce216b5a7068a6124ea
Kyle <kyle.yk@outlook.com>
2022-03-28 22:56:47 +0800

    refactor: codeclean for GlobalState

--- src/main/model/GlobalState.java
+++ src/main/model/GlobalState.java
@@ -1,11 +1,5 @@
 package main.model;
 
-import java.util.List;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GlobalState {
