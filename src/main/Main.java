a036484431d8549e91d43181331785aa8ac71faf
Kyle <kyle.yk@outlook.com>
2022-04-08 23:58:20 +0800

    refactor: documentation fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -41,7 +41,7 @@ public class Main extends Application {
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts
+     * launched through deployment artifacts.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
