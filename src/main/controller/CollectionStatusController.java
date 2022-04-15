704f107ddbacca16523870d7240ca304534a9f37
Kyle <kyle.yk@outlook.com>
2022-04-16 00:23:33 +0800

    feat: Collection Page (Poly) -> Ngo & Donor

--- src/main/controller/CollectionStatusController.java
+++ src/main/controller/CollectionStatusController.java
@@ -20,6 +20,7 @@ import javafx.stage.Stage;
 import main.model.AidList;
 import main.model.Database;
 import main.model.Donor;
+import main.model.Ngo;
 import main.model.GlobalState;
 
 public class CollectionStatusController implements Initializable{
@@ -43,13 +44,7 @@ public class CollectionStatusController implements Initializable{
     private Button backBtn;
 
     private GlobalState state = GlobalState.getInstance();
-    private Donor donorUserInfo = state.getDonorSession();
-    private String identity = donorUserInfo.getIdentity();
-    private String username = donorUserInfo.getUsername();
-
-
-    String DataFileName = state.getDataFileName(identity);
-    List<List<String>> Acc_Info = Database.readData(DataFileName);
+    private String identity = state.getIdentity();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -69,13 +64,30 @@ public class CollectionStatusController implements Initializable{
         statusCol.setCellValueFactory(new PropertyValueFactory<AidList, String>("status"));
 
         List<List<String>> DataList = Database.readData("distributed_Info");
-        if (DataList != null) {
-            for (List<String> Data: DataList) {
-                for(int i=0; i < Acc_Info.size(); i++){
-                    System.out.println(Acc_Info.get(i).get(2));
-                    //System.out.println(Data.get(0));
-                    //System.out.println(donorUserInfo.getUsername());
-                    if(Data.get(0).equals(Acc_Info.get(i).get(2))){
+
+        if (identity == "Donor"){
+            Donor donorUserInfo = state.getDonorSession();
+            if (DataList != null) {
+                for (List<String> Data: DataList) {
+                    if(Data.get(0).equals(donorUserInfo.getName())){
+                    AidList info = new AidList(Data.get(0), 
+                                                Data.get(1), 
+                                                Data.get(2), 
+                                                Integer.parseInt(Data.get(3)), 
+                                                Data.get(4), 
+                                                Integer.parseInt(Data.get(5)),
+                                                Data.get(6));
+                    collectionStatus.add(info);
+                    }
+                }
+            }
+
+        }
+        else if (identity == "NGO"){
+            Ngo ngoUserInfo = state.getNgoSession();
+            if (DataList != null) {
+                for (List<String> Data: DataList) {
+                    if(Data.get(4).equals(ngoUserInfo.getName())){
                     AidList info = new AidList(Data.get(0), 
                                                 Data.get(1), 
                                                 Data.get(2), 
@@ -85,18 +97,25 @@ public class CollectionStatusController implements Initializable{
                                                 Data.get(6));
                     collectionStatus.add(info);
                 
+                    }
                 }
             }
-            }
+
         }
 
         statusTable.setItems(collectionStatus);
     }
 
-    public void switchToDonatePage(){
+    public void switch_to_backPage(){
+        String viewPath = "";
+        if (identity == "Donor")
+            viewPath = "/main/view/DonatePage.fxml";
+        else if (identity == "NGO")
+            viewPath = "/main/view/RequestPage.fxml";
+
         try{
             Stage mainStage = GlobalState.getInstance().getStage();
-            Parent root = FXMLLoader.load(getClass().getResource("/main/view/DonatePage.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource(viewPath));
             mainStage.setScene(new Scene(root, 1280, 720));
         }catch (IOException ioe){
             ioe.printStackTrace();
